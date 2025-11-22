4ed1def853a0e60a89aee87b8316c41393fb6da4
charlesiq <cvibanezq@gmail.com>
2025-11-22 20:10:50 -0300

    arreglos hito pasado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:4173",
+    origin: "http://localhost:5173",
     credentials: true, 
     exposedHeaders: ["X-CSRF-Token"],
   })
